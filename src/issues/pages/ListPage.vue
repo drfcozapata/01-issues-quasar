7c6ca515e388f422443d5203dd548f7a44a850d3
Francisco Zapata <drfcozapata@gmail.com>
2024-07-12 11:42:26 -0400

    First commit: start app ready

---
+++ src/issues/pages/ListPage.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+//
+</script>
+
+<template>
+  <span class="text-h2">ListPage</span>
+</template>
+
+<style scoped>
+/* */
+</style>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import EssentialLink, {
+  EssentialLinkProps,
+} from 'components/EssentialLink.vue';
+
+defineOptions({
+  name: 'MainLayout',
+});
+
+const linksList: EssentialLinkProps[] = [
+  {
+    title: 'Docs',
+    caption: 'quasar.dev',
+    icon: 'school',
+    link: 'https://quasar.dev',
+  },
+];
+
+const leftDrawerOpen = ref(false);
+
+function toggleLeftDrawer() {
+  leftDrawerOpen.value = !leftDrawerOpen.value;
+}
+</script>
+
+<template>
+  <q-layout view="hHh Lpr lFf">
+    <q-header>
+      <q-toolbar class="bg-dark text-white">
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title> Issues App </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" bordered>
+      <q-list>
+        <q-item-label header> Essential Links </q-item-label>
+
+        <EssentialLink
+          v-for="link in linksList"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
